7c412eb37db36e089d1ea94f4030bb546d6e97be
Over017 <felipe.amaru.venegas@gmail.com>
2026-04-30 21:05:03 -0400

    Coneccion back and front
    se establece una conexion con el frot

---
+++ FrontEnd/script.js
@@ -0,0 +1,46 @@
+async function buscarCuenta() {
+
+    const email =
+        document.getElementById("email").value;
+
+    const resultados =
+        document.getElementById("resultados");
+
+    resultados.innerHTML = "<p>Buscando...</p>";
+
+    try {
+
+        const response = await fetch(
+            `http://127.0.0.1:5000/buscar?email=${email}`
+        );
+
+        const data = await response.json();
+
+        resultados.innerHTML = "";
+
+        if(data.length === 0) {
+
+            resultados.innerHTML =
+                "<p>No se encontraron cuentas</p>";
+
+            return;
+        }
+
+        data.forEach(cuenta => {
+
+            resultados.innerHTML += `
+                <div class="result-card">
+                    <h3>${cuenta.plataforma}</h3>
+                    <p>${cuenta.email}</p>
+                </div>
+            `;
+        });
+
+    } catch(error) {
+
+        resultados.innerHTML =
+            "<p>Error conectando con backend</p>";
+
+        console.error(error);
+    }
+}
